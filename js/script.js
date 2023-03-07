b0b3ec96545e115aa252b3966335f260bd654407
cristianLucianPopescu <91139455+cristianLucianPopescu@users.noreply.github.com>
2023-03-07 13:20:40 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
+// INDEX PAGE
+
 var i = 0;
 var txt = 'Create Strong Passwords';
 var speed = 120;
 
+// function to auto-type text
 function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("title").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
+    if (i < txt.length) {
+        document.getElementById("title").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
 }
-
