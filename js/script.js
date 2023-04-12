3aebf77c64acf7e1b3d5a8277bd2ecf1a0feb1ca
cristianLucianPopescu <91139455+cristianLucianPopescu@users.noreply.github.com>
2023-04-12 08:49:46 +0100

    added consent form, link to survey etc.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
+// INDEX PAGE
+
 var i = 0;
 var txt = 'Create Strong Passwords';
 var speed = 120;
 
+// function to auto-type text
 function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("title").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
+    if (i < txt.length) {
+        document.getElementById("title").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
 }
 
+// open and close the consent form
+function openConsent() {
+    document.getElementById("consentForm").style.display = "block";
+}
+
+function closeConsent() {
+    document.getElementById("consentForm").style.display = "none";
+}
+
+function showPopup() {
+    document.getElementById("popup-text").style.display = "block";
+}
